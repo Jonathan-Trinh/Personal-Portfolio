b3473bf7526ef5578682f73667b89cd4333dcf99
Jonathan Trinh <jonathantrinh@Jonbook-Pro.local>
2024-07-17 22:42:10 -0700

    Finished Experience and About components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import styles from './App.module.css'
-import { Hero } from './components/Navbar/Hero/Hero';
+import { About } from './components/About/About';
+import { Experience } from './components/Experience/Experience';
+import { Hero } from './components/Hero/Hero';
 import { Navbar } from './components/Navbar/Navbar'
 
 function App() {
@@ -7,6 +9,8 @@ function App() {
     <div className={styles.App}>
       <Navbar />
       <Hero />
+      <About/>
+      <Experience/>
     </div>
   );
 }
